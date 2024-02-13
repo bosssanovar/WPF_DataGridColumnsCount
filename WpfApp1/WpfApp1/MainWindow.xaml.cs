b3bc0a4748fa78bbdc1158fd21050953e7e14e80
kei ito <ito.kei@jvckenwood.com>
2024-02-13 11:04:31 +0900

    first commit

---
+++ WpfApp1/WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,67 @@
+﻿using Reactive.Bindings;
+using System;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public ObservableCollection<Object> Items { get; } = new ObservableCollection<Object>();
+
+        public MainWindow()
+        {
+            for (int i = 0; i < 200; i++)
+            {
+                Items.Add(new Object());
+            }
+
+            InitializeComponent();
+        }
+
+        protected override void OnContentRendered(EventArgs e)
+        {
+            base.OnContentRendered(e);
+
+            Cursor = Cursors.Wait;
+
+            for (int columnIndex = 0; columnIndex < 200; ++columnIndex)
+            {
+
+                var factory = new FrameworkElementFactory(typeof(Rectangle));
+                factory.SetValue(Rectangle.HeightProperty, 10.0);
+                factory.SetValue(Rectangle.WidthProperty, 10.0);
+                factory.SetValue(Rectangle.FillProperty, Brushes.LightSkyBlue);
+
+                var dataTemplate = new DataTemplate();
+                dataTemplate.VisualTree = factory;
+
+                var column = new DataGridTemplateColumn();
+                column.CellTemplate = dataTemplate;
+
+                grid.Columns.Add(column);
+            }
+
+            grid.Visibility = Visibility.Visible;
+
+            Dispatcher.InvokeAsync(new Action(() =>
+            {
+                Cursor = null;
+            }), System.Windows.Threading.DispatcherPriority.ApplicationIdle);
+        }
+    }
+}
